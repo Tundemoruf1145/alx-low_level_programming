d7d89f9511f6023c541991a15c53618bb8590c3e
Tundemoruf1145 <tundemoruf@gmail.com>
2022-10-25 06:00:54 -0700

    4-Task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: a string
+ *
+ * Return: no return.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
